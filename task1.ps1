1eec41a648b100015ba0fae04b24656410daf997
ualix1986 <118163922+ualix1986@users.noreply.github.com>
2022-11-14 14:03:19 +0600

    Add files via upload

---
+++ task1.ps1
@@ -0,0 +1,35 @@
+﻿Function Test-Subnet {
+
+param (
+
+     [parameter(Mandatory=$true)]
+
+     [Net.IPAddress]
+
+     $ipaddress1,
+
+ 
+
+     [parameter(Mandatory=$true)]
+
+     [Net.IPAddress]
+
+     $ipaddress2,
+
+ 
+
+     [parameter(Mandatory=$true)]
+
+     [Net.IPAddress]
+
+     $mask
+
+)
+
+ 
+
+($ipaddress1.address -band $mask.address) -eq ($ipaddress2.address -band $mask.address)
+
+ 
+
+}
